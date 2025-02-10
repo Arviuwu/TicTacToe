440080fe93a47c0972f083702bd13cee761ac859
Arviuwu <arviu23@gmail.com>
2025-02-10 21:00:33 +0100

    left vertical win con

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -2,19 +2,27 @@
 string coordinateInput = "";
 int coordinate1 = 0;
 int coordinate2 = 0;
+bool pXTurn = true;
+bool pOTurn = false;
+string pXSign = "X";
+string pOSign = "O";
+string error = "Unexpected error";
+bool win = false;
+int wincountPX = 0;
+int wincountPO = 0;
 
 string[,] board =
 {
-    {"1", "2", "3" },
-    {"4", "5", "5" },
-    {"7", "8", "9" }
-}; 
-    
-    
-    
+    {"X", " ", " " },
+    {"X", " ", " " },
+    {"X", " ", " " }
+};
+
 
 while (repeat)
 {
+    Console.Clear();
+
     //updates and draws board
     Console.WriteLine($"   -----------");
     Console.WriteLine($"1 | {board[0, 0]} | {board[0, 1]} | {board[0, 2]} |");
@@ -25,23 +33,103 @@ while (repeat)
     Console.WriteLine($"   -----------");
     Console.WriteLine($"    A   B   C");
 
-    Console.WriteLine("Enter coordinates");
-    coordinateInput = Console.ReadLine().ToUpper();
-    
+    // marker placement loop
+    while (true)
+    {
+        Console.WriteLine("Enter coordinates");
+        coordinateInput = Console.ReadLine().ToUpper();
 
-    if (!coordinateInput.Contains("A") || !coordinateInput.Contains("B") || !coordinateInput.Contains("C") || !coordinateInput.Contains("1") || !coordinateInput.Contains("2") || !coordinateInput.Contains("3"))
-    {
-        Console.WriteLine("oaiwhfoa");
-    }
-    else
-    {
+
         coordinate1 = coordinateInput[0] - '1';
         coordinate2 = coordinateInput[1] - 'A';
-        Console.WriteLine(board[coordinate1, coordinate2]);
-        
+
+        if (board[coordinate1, coordinate2] == " ")
+        {
+            if (pXTurn)
+            {
+
+                board[coordinate1, coordinate2] = pXSign;
+                pXTurn = false;
+                pOTurn = true;
+                break;
+            }
+            else if (pOTurn)
+            {
+                board[coordinate2, coordinate1] = pOSign;
+                pXTurn = true;
+                pOTurn = false;
+                break;
+            }
+            else
+            {
+                Console.WriteLine(error);
+            }
+        }
+        else
+        {
+            Console.WriteLine("already guessed");
+        }
     }
-    
-    
-    
+
+    // win check    
+
+    for (int i = 0; i < 3; i++) //left vertical line  
+    {
+        if (board[i, 0] != " ")
+        {
+            if(board[i, 0] == pXSign)
+            {
+                wincountPX++;
+            }
+            else if (board[i, 0] == pOSign)
+            {
+                wincountPO++;
+            }
+        }
+    }
+    if (wincountPX == 3) 
+    {
+        Console.WriteLine("Player X won");
+    }
+    else if (wincountPO == 3)
+    {
+        Console.WriteLine("Player O won");
+    }
+    Console.WriteLine(wincountPX);
+    Console.WriteLine(wincountPO);
+    wincountPX = 0;
+    wincountPO = 0;
+
+
+    /*for (int i = 0; i < 3; i++) //middle vertical line  
+    {
+        if (board[i, 1] != " ")
+        {
+            if (board[i, 1] == P1Sign)
+            {
+                wincountP1++;
+            }
+            else
+            {
+                wincountP2++;
+            }
+        }
+    }
+    if (wincountP1 == 3)
+    {
+        Console.WriteLine("Player 1 won");
+    }
+    else if (wincountP2 == 3)
+    {
+        Console.WriteLine("Player 2 won");
+    }
+    wincountP1 = 0;
+    wincountP2 = 0;*/
+
     Console.ReadKey();
-}
+}    
+    
+    
+    
+ 
+
